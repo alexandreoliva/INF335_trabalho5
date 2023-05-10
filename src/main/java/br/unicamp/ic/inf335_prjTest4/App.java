0d2dda4a449ec9e364c3da29def57b8c95388eee
alexandreoliva <alexandreoliva@gmail.com>
2023-05-10 00:06:45 -0300

    second commit

--- src/main/java/br/unicamp/ic/inf335_prjTest4/App.java
+++ src/main/java/br/unicamp/ic/inf335_prjTest4/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	// TODO Auto-generated method stub
+    	// TODO Auto-generated method stub !!
 		
     			AnuncioBean anuncio = new AnuncioBean();
     			ProdutoBean produto = new ProdutoBean();
